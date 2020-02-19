a274e27a7af53131e0b2ba94c127b6f9fa85bc8b
Steve Shreeve <steve.shreeve@gmail.com>
2020-02-19 02:51:35 -0700

    Initial cleanup

--- lib/googlesheets.rb
+++ lib/googlesheets.rb
@@ -1,68 +1,63 @@
 $stdout.sync = true
 
-require 'google/apis/sheets_v4'
-require 'googleauth'
-require 'googleauth/stores/file_token_store'
+require "bundler/setup"
 
-APPLICATION_NAME ||= 'Google Sheets Ruby gem'.freeze
-SCOPE            ||= Google::Apis::SheetsV4::AUTH_SPREADSHEETS
-CREDENTIALS_PATH ||= File.join($APP_ROOT, 'credentials.json').freeze
-TOKEN_PATH       ||= File.join($APP_ROOT, 'token.yaml').freeze
-OOB_URI          ||= 'urn:ietf:wg:oauth:2.0:oob'.freeze
+require "google/apis/sheets_v4"
+require "googleauth"
+require "googleauth/stores/file_token_store"
 
-# Google::Apis.logger = Logger.new(STDERR)
-# Google::Apis.logger.level = Logger::DEBUG
+module Enumerable
+  def first_result
+    block_given? ? each {|item| result = (yield item) and return result} && nil : find {|item| item}
+  end
+end
 
-helpers do
-  def google
-    $google ||= begin
-      connect = -> {
-        idno = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
-        repo = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
-        auth = Google::Auth::UserAuthorizer.new(idno, SCOPE, repo)
-        user = 'default'
-        credentials = auth.get_credentials(user) || begin
-          href = auth.get_authorization_url(base_url: OOB_URI)
-          puts "Open the following URL and paste the code here:\n" + href
-          code = gets
-          credentials = auth.get_and_store_credentials_from_code(
-            user_id: user, code: code, base_url: OOB_URI
-          )
-        end
-      }
-      $google = Google::Apis::SheetsV4::SheetsService.new
-      $google.client_options.application_name = APPLICATION_NAME
-      $google.authorization = connect.call
-      $google
+class GoogleSheets
+  attr_accessor :api
+
+  def initialize(ssid, *args)
+    # Google::Apis.logger = Logger.new(STDERR)
+    # Google::Apis.logger.level = Logger::DEBUG
+
+    @ssid = ssid
+
+    @json = 'credentials.json'
+    @yaml = 'token.yaml'
+
+    @api = Google::Apis::SheetsV4::SheetsService.new
+    @api.client_options.application_name    = "Ruby"
+    @api.client_options.application_version = "1.0.0"
+    @api.authorization = authorize
+  end
+
+  def authorize
+    idno = Google::Auth::ClientId.from_file(@json)
+    repo = Google::Auth::Stores::FileTokenStore.new(file: @yaml)
+    auth = Google::Auth::UserAuthorizer.new(idno, Google::Apis::SheetsV4::AUTH_SPREADSHEETS, repo)
+    oobs = 'urn:ietf:wg:oauth:2.0:oob'
+    user = 'default'
+    info = auth.get_credentials(user) || begin
+      href = auth.get_authorization_url(base_url: oobs)
+      puts "Open the following URL and paste the code here:\n" + href
+      info = auth.get_and_store_credentials_from_code(user_id: user, code: gets, base_url: oobs)
     end
   end
 
-  def sheet_list
-    list = google.get_spreadsheet($link).sheets
-    list.map do |item|
-      vals = item.properties
-      {
-        id:    vals.sheet_id,
-        name:  vals.title,
-        color: rgb2hex(vals.tab_color),
-      }.compact
-    end
+  def rgb2hex(color=nil)
+    color or return
+    r = ((color.red   || 0) * 255).to_i
+    g = ((color.green || 0) * 255).to_i
+    b = ((color.blue  || 0) * 255).to_i
+    "#%02x%02x%02x" % [r, g, b]
   end
 
-  def sheet_id(obj)
-    case obj
-      when /^#(\d+)$/ then $list[$1.to_i - 1].sheet_id
-      when Integer    then obj
-      else $list.first_result {|item| item.sheet_id if item.title == obj}
-    end
-  end
-
-  def sheet_name(obj)
-    case obj
-      when /^#(\d+)$/ then $list[$1.to_i - 1].title
-      when Integer    then $list.first_result {|item| item.title if item.sheet_id == obj}
-      else obj
-    end
+  def hex2rgb(color=nil)
+    color =~ /\A#?(?:(\h\h)(\h\h)(\h\h)|(\h)(\h)(\h))\z/ or return
+    r, g, b = $1 ? [$1, $2, $3] : [$4*2, $5*2, $6*2]
+    r = "%.2f" % (r.hex / 255.0)
+    g = "%.2f" % (g.hex / 255.0)
+    b = "%.2f" % (b.hex / 255.0)
+    { red: r, green: g, blue: b }
   end
 
   def biject(x) # a:1, z:26, aa:27, az:52, ba:53, aaa:703
@@ -90,32 +85,52 @@ helpers do
     }.compact
   end
 
-  def rgb2hex(color=nil)
-    color or return
-    r = ((color.red   || 0) * 255).to_i
-    g = ((color.green || 0) * 255).to_i
-    b = ((color.blue  || 0) * 255).to_i
-    "#%02x%02x%02x" % [r, g, b]
+  def sheets
+    @sheets ||= @api.get_spreadsheet(@ssid).sheets.map {|item| item.properties }
   end
 
-  def hex2rgb(color=nil)
-    color =~ /\A#?(\h\h)(\h\h)(\h\h)\z/i or return
-    r = "%.2f" % ($1.hex / 255.0)
-    g = "%.2f" % ($2.hex / 255.0)
-    b = "%.2f" % ($3.hex / 255.0)
-    { red: r, green: g, blue: b }
+  def sheets!
+    @sheets = nil
+    sheets
   end
 
-  def sheet_color(sheet, color=nil) # NOTE: ignores alpha
+  def sheet_list
+    sheets.map do |item|
+      {
+        id:    item.sheet_id,
+        name:  item.title,
+        color: rgb2hex(item.tab_color),
+      }.compact
+    end
+  end
+
+  def sheet_id(obj)
+    case obj
+      when /^#(\d+)$/ then sheets[$1.to_i - 1].sheet_id
+      when Integer    then obj
+      else sheets.first_result {|item| item.sheet_id if item.title == obj}
+    end
+  end
+
+  def sheet_name(obj)
+    case obj
+      when /^#(\d+)$/ then sheets[$1.to_i - 1].title
+      when Integer    then sheets.first_result {|item| item.title if item.sheet_id == obj}
+      else obj
+    end
+  end
+
+  def sheet_color(pick, color=nil) # NOTE: ignores alpha
     reqs = []
     reqs.push(update_sheet_properties: {
       properties: {
-        sheet_id: sheet_id(sheet),
+        sheet_id: sheet_id(pick),
         tab_color: hex2rgb(color),
       },
       fields: 'tab_color',
     })
-    resp = google.batch_update_spreadsheet($link, { requests: reqs }, {})
+    resp = @api.batch_update_spreadsheet(@ssid, { requests: reqs }, {})
+    true
   end
 
   def sheet_format(area, pattern)
@@ -132,35 +147,97 @@ helpers do
       },
       fields: 'user_entered_format.number_format',
     })
-    resp = google.batch_update_spreadsheet($link, { requests: reqs }, {})
+    resp = @api.batch_update_spreadsheet(@ssid, { requests: reqs }, {})
+    true
+  end
+
+  def sheet_clear(area)
+    @api.clear_values(@ssid, area)
   end
 
   def sheet_read(area)
-    google.get_spreadsheet_values($link, area).values
-  end
-  def read(area)
-    google.get_spreadsheet_values($link, area).values
+    @api.get_spreadsheet_values(@ssid, area).values
   end
 
-  def clear(area)
-    google.clear_values($link, area)
-  end
-
-  def sheet_save(area, rows)
-    gasv = Google::Apis::SheetsV4::ValueRange.new(range: area, values: rows)
-    done = google.update_spreadsheet_value($link, area, gasv, value_input_option: "USER_ENTERED")
-    done.updated_cells
-  end
-  def save(area, rows, log=true)
+  def sheet_save(area, rows, log=false)
     area.sub!(/^(#\d+)(?=!)/) {|num| sheet_name(num)}
     gasv = Google::Apis::SheetsV4::ValueRange.new(range: area, values: rows)
-    done = google.update_spreadsheet_value($link, area, gasv, value_input_option: "USER_ENTERED")
+    done = @api.update_spreadsheet_value(@ssid, area, gasv, value_input_option: "USER_ENTERED")
     puts "#{done.updated_cells} cells updated." if log
-    rows.count
+    done.updated_cells
   end
-  def save!(area, rows, log=false)
-    save(area, rows, log)
+
+  def sheet_save!(area, rows)
+    sheet_save(area, rows, true)
   end
+
+  def sheet_filter(area)
+    range = range(area)
+    reqs = []
+    reqs.push(clear_basic_filter: { sheet_id: range[:sheet_id] })
+    reqs.push(set_basic_filter: { filter: { range: range } })
+    resp = @api.batch_update_spreadsheet(@ssid, { requests: reqs }, {})
+    true
+  end
+end
+
+g = GoogleSheets.new('1OQ4FoV8gSxCsCRJULP4eFV2rKLXNe8CnSMEN4IJ55jE')
+
+p g.sheet_list
+p g.sheet_name("#1")
+p g.sheet_id("#1")
+p g.sheets
+p g.sheet_color "#1", "#0f0"
+p g.sheet_filter "#1!C15:e26"
+
+__END__
+
+# append_spreadsheet_value
+# apply_command_defaults
+# batch
+# batch_clear_values
+# batch_get_spreadsheet_values
+# batch_spreadsheet_value_clear_by_data_filter
+# batch_spreadsheet_value_get_by_data_filter
+# batch_spreadsheet_value_update_by_data_filter
+# batch_update_spreadsheet
+# batch_update_values
+# batch_upload
+# clear_values
+# copy_spreadsheet
+# create_spreadsheet
+# execute_or_queue_command
+# fetch_all
+# get_spreadsheet
+# get_spreadsheet_by_data_filter
+# get_spreadsheet_developer_metadatum
+# get_spreadsheet_values
+# http
+# logger
+# make_download_command
+# make_simple_command
+# make_upload_command
+# search_developer_metadatum_developer_metadata
+# update_spreadsheet_value
+# --
+# authorization@
+# base_path@
+# batch_path@
+# client@
+# client_options@
+# key@
+# quota_user@
+# request_options@
+# root_url@
+# upload_path@
+
+# add_dimension_group_request                   = Google::Apis::SheetsV4::AddDimensionGroupRequest.new
+# add_dimension_group_request.range             = Google::Apis::SheetsV4::DimensionRange.new
+# add_dimension_group_request.range.sheet_id    = 0
+# add_dimension_group_request.range.dimension   = "ROWS"
+# add_dimension_group_request.range.start_index = 2
+# add_dimension_group_request.range.end_index   = 4
+# batch_update_spreadsheet_request.requests.push Google::Apis::SheetsV4::Request.new(add_dimension_group: add_dimension_group_request)
 
   def touch_cell(cell, func=nil, *args, **opts, &code)
     return yield(cell) if block_given?
@@ -224,11 +301,11 @@ helpers do
     end
   end
 
-  def update(area, op=nil, **opts, &code)
-    rows = read area
-    data = rows.map {|cols| cols.map {|cell| touch_cell(cell, op, **opts, &code)}}
-    save area, data
-  end
+  # def update(area, op=nil, **opts, &code)
+  #   rows = read area
+  #   data = rows.map {|cols| cols.map {|cell| touch_cell(cell, op, **opts, &code)}}
+  #   save area, data
+  # end
 
   def sheet_clean(area)
     rows = sheet_read(area) rescue {}
@@ -361,56 +438,4 @@ def report1(area)
   rows
 end
 
-
-# workbook and worksheets
-$link = "2I7WROLLau2h7MyqVIJxGzKcR7PiM8_xgc79Q1F_cfwM"
-$list = google.get_spreadsheet($link).sheets.map {|s| s.properties }
-
 report1("#1!B25"); save "#1!P2", [[Time.now.strftime("%b %-0d, %Y @ %-0I:%M%P MT")]]
-
-__END__
-
-def basic_filter(sheet_id, count)
-  row =  8 # row index (this is the filter header row number)
-  col = 21 # col index (this is the filter header right col) 'o' gets 15 ('o'.ord - 'a'.ord + 1)
-
-  batch_update_spreadsheet_request                         = Google::Apis::SheetsV4::BatchUpdateSpreadsheetRequest.new
-  batch_update_spreadsheet_request.requests                = []
-
-  clear_basic_filter_request                               = Google::Apis::SheetsV4::ClearBasicFilterRequest.new
-  clear_basic_filter_request.sheet_id                      = sheet_id
-  batch_update_spreadsheet_request.requests.push Google::Apis::SheetsV4::Request.new(clear_basic_filter: clear_basic_filter_request)
-
-  set_basic_filter_request                                 = Google::Apis::SheetsV4::SetBasicFilterRequest.new
-  set_basic_filter_request.filter                          = Google::Apis::SheetsV4::BasicFilter.new
-  set_basic_filter_request.filter.range                    = Google::Apis::SheetsV4::GridRange.new
-  set_basic_filter_request.filter.range.sheet_id           = sheet_id
-  set_basic_filter_request.filter.range.start_row_index    = row - 1
-  set_basic_filter_request.filter.range.start_column_index = 1
-  set_basic_filter_request.filter.range.end_row_index      = row + count
-  set_basic_filter_request.filter.range.end_column_index   = col
-  batch_update_spreadsheet_request.requests.push Google::Apis::SheetsV4::Request.new(set_basic_filter: set_basic_filter_request)
-
-  # add_dimension_group_request                   = Google::Apis::SheetsV4::AddDimensionGroupRequest.new
-  # add_dimension_group_request.range             = Google::Apis::SheetsV4::DimensionRange.new
-  # add_dimension_group_request.range.sheet_id    = 0
-  # add_dimension_group_request.range.dimension   = "ROWS"
-  # add_dimension_group_request.range.start_index = 2
-  # add_dimension_group_request.range.end_index   = 4
-  # batch_update_spreadsheet_request.requests.push Google::Apis::SheetsV4::Request.new(add_dimension_group: add_dimension_group_request)
-
-  # add_dimension_group_request                   = Google::Apis::SheetsV4::AddDimensionGroupRequest.new
-  # add_dimension_group_request.range             = Google::Apis::SheetsV4::DimensionRange.new
-  # add_dimension_group_request.range.sheet_id    = 0
-  # add_dimension_group_request.range.dimension   = "ROWS"
-  # add_dimension_group_request.range.start_index = 13
-  # add_dimension_group_request.range.end_index   = 17
-  # batch_update_spreadsheet_request.requests.push Google::Apis::SheetsV4::Request.new(add_dimension_group: add_dimension_group_request)
-
-  google.batch_update_spreadsheet($url, batch_update_spreadsheet_request)
-end
-
-area = 'Report!B9:K'
-
-count = report area
-basic_filter $list[0].sheet_id, count
